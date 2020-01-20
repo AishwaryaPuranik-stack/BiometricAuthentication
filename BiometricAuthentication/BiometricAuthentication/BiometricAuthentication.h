72f5efa9aaf93efb1765169be6d9b58f2b3d85a9
AishwaryaPuranik-stack <aishu_puranik@outlook.com>
2020-01-20 16:46:37 +0530

    Finished

---
+++ BiometricAuthentication/BiometricAuthentication/BiometricAuthentication.h
@@ -0,0 +1,19 @@
+//
+//  BiometricAuthentication.h
+//  BiometricAuthentication
+//
+//  Created by Aishwarya Krishna Puranik on 20/01/20.
+//  Copyright © 2020 Aishwarya Krishna Puranik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BiometricAuthentication.
+FOUNDATION_EXPORT double BiometricAuthenticationVersionNumber;
+
+//! Project version string for BiometricAuthentication.
+FOUNDATION_EXPORT const unsigned char BiometricAuthenticationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BiometricAuthentication/PublicHeader.h>
+
+
---
+++ BiometricAuthentication/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  BiometricAuthentication
+//
+//  Created by Aishwarya Krishna Puranik on 20/01/20.
+//  Copyright © 2020 Aishwarya Krishna Puranik. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+        return "Hey!"
+    }
+}
